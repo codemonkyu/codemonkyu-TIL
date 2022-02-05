10427c816fa1f6c90c19232b2f9a765810b824fa
codemonkyu <codemonkyu@gmail.com>
2022-02-06 02:58:37 +0900

    Create 6244.py

--- swea/6244.py
+++ swea/6244.py
@@ -1,2 +1,16 @@
 scores = [85, 65, 77, 83, 75, 22, 98, 88, 38, 100]
 
+i=0
+sum_score=0
+
+while(i<10):
+    if(scores[i]>=80):
+        sum_score += scores[i]
+        
+    else:
+        pass
+    
+    i += 1
+    
+print(sum_score)
+        
