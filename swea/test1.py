76c8fc1096f1a33281a5fb0f8a955fdeddec322d
codemonkyu <codemonkyu@gmail.com>
2022-02-12 23:19:06 +0900

    study pthon

---
+++ swea/test1.py
@@ -0,0 +1,28 @@
+
+# a = [1.2, 1.3, 1.5, 1.7]
+
+# a = list(map(int,a))
+
+# print(a)
+
+
+
+# a = list(map(str,range(11)))
+# print(a)
+
+
+# a = list(map(int,range(11)))
+# print(a)
+
+# a = list(map(float,range(11)))
+
+# print(a)
+
+
+# a = input().split()
+
+# print(a)
+
+a = list(map(int, input().split()))
+
+print(a)
