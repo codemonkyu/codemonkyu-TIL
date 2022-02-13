47c6aa40f691f0afff70139acd022dbc79b61b74
codemonkyu <codemonkyu@gmail.com>
2022-02-14 02:57:19 +0900

    Add customermodels.py

--- mydjango_test/mydjango/customerprj/customer/models.py
+++ mydjango_test/mydjango/customerprj/customer/models.py
@@ -11,9 +11,18 @@ class customer(models.Model):
         ('gay','gay'),
         ('transgender','transgender'),
     }
+    # choice (저장값,디스플레이)
+    
     name = models.CharField(max_length = 20)
     birthdate = models.DateField()
     text = models.TextField(max_length = 100, default=("introduce your self"))
     email = models.EmailField(max_length = 50, default=("@"))
     gender = models.CharField(max_length = 40, choices=CATEGORY_CHOICES)
     Join_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    
+    def publish(self):
+        self.join_date = timezone.now()
+        self.save()
