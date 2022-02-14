a53c69acde8ab24503ad8e4907bfdc7989f5b769
codemonkyu <codemonkyu@gmail.com>
2022-02-14 20:59:02 +0900

    Create customer

---
+++ mydjango_test/customerprj/customer/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('customer_list/', views.customer_list, name='customer_list'),
+]
---
+++ mydjango_test/customerprj/customer/views.py
@@ -0,0 +1,18 @@
+import email
+from unicodedata import name
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+from .models import customer
+
+def customer_list(request):
+    my_name = '장고프레임워크'
+    http_method = request.method
+    customers = customer.objects.all()
+    context = { 'customers': customers,}
+    return render(request, 'customer/customer_list.html', context)
+
+
+#     return render(request, 'customer/customer_list.html', {'customer_info': customers},)
+
+
