a5bb2795e68be649988bd67ce80c075f49db169b
codemonkyu <codemonkyu@gmail.com>
2022-02-19 03:07:44 +0900

    create login.html

--- Django/myProject/discord/views.py
+++ Django/myProject/discord/views.py
@@ -1,5 +1,6 @@
 import re
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -13,9 +14,15 @@ from .forms import RoomForm
         
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
     topics = Topic.objects.all()
-    context = {'rooms':rooms, 'topics':topics}
+    room_count = rooms.count()
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
