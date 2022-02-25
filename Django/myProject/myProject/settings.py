7fb68e9320c0548b6575691af577a59e23beb9b7
codemonkyu <codemonkyu@gmail.com>
2022-02-25 16:02:50 +0900

    Create project

--- Django/myProject/myProject/settings.py
+++ Django/myProject/myProject/settings.py
@@ -39,11 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'discord.apps.DiscordConfig',
+    
+     'rest_framework',
+     'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -131,3 +135,5 @@ STATICFILES_DIRS = [
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+
+CORS_ALLOW_ALL_ORIGINS = True
