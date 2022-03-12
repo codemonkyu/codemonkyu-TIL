46d5dc64971b56297e71209204f8931ddb45577a
codemonkyu <codemonkyu@gmail.com>
2022-03-12 21:40:35 +0900

    Create: feed 자동화 및 migrate로 db 쿼리 만듬

--- mydjango_test/instagram_prj/junstagram/content/views.py
+++ mydjango_test/instagram_prj/junstagram/content/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
 
-# Create your views here.
+
+
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all() #이게 몰 뜻함? select * from content_feed;
+        
+        
+        context={'feed_list':feed_list}
+        return render(request, "junstagram/main.html", context)
+
+
+
+
+
+
+
+
