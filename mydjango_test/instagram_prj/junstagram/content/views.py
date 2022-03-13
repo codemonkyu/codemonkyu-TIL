df58c710e6df5760726180dbef25f7f0ffbd45a1
codemonkyu <codemonkyu@gmail.com>
2022-03-13 19:25:40 +0900

    Create : feed 처음부터 다시 만듬

--- mydjango_test/instagram_prj/junstagram/content/views.py
+++ mydjango_test/instagram_prj/junstagram/content/views.py
@@ -8,7 +8,7 @@ class Main(APIView):
     def get(self, request):
         feed_list = Feed.objects.all() #이게 몰 뜻함? select * from content_feed;
         
-        
+    
         context={'feed_list':feed_list}
         return render(request, "junstagram/main.html", context)
 
