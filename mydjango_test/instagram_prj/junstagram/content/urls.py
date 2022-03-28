c985cad2f99a369e5e00c58d1c778cc5d598b9b1
codemonkyu <codemonkyu@gmail.com>
2022-03-28 23:42:19 +0900

    Create : 회원가입페이지

--- mydjango_test/instagram_prj/junstagram/content/urls.py
+++ mydjango_test/instagram_prj/junstagram/content/urls.py
@@ -13,20 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from content.views import Main, UploadFeed
-from django.conf.urls.static import static
-from .settings import MEDIA_ROOT, MEDIA_URL
+
+from django.urls import path
+from content.views import UploadFeed
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('main/', Main.as_view()), 
-    path("content/",include('content.urls')),
-    path("user/",include('user.urls')),
+    path('upload/', UploadFeed.as_view()),
+
 ]
 
 
 
 
-urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
