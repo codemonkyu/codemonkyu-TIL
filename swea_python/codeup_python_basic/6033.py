6efb1870e8919bdbbb2e7b7d8de35ad2f7e56182
codemonkyu <codemonkyu@gmail.com>
2022-04-19 11:36:46 +0900

    study 4/19

---
+++ swea_python/codeup_python_basic/6033.py
@@ -0,0 +1,6 @@
+a = input()
+b = ord(a) + 1
+n = chr(b)
+
+print(n) 
+print('test push')
---
+++ swea_python/codeup_python_basic/6044.py
@@ -0,0 +1,8 @@
+a, b = map(int, input().split())
+
+print(f'{a+b}')
+print(f'{a-b}')
+print(f'{a*b}')
+print(f'{a//b}')
+print(f'{a%b}')
+print(f'{a/b:0.2f}')
