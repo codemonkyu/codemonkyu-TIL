95fa11546426295fe86a5238522744b257398c2f
codemonkyu <codemonkyu@gmail.com>
2022-08-02 23:18:03 +0900

    8.2

--- python/초보자를 위한 파이썬 300제/반복문.py
+++ python/초보자를 위한 파이썬 300제/반복문.py
@@ -88,3 +88,14 @@ for i in 리스트:
     j = i.split(".")
     if j[1] == "h":
         print(i)
+        
+#160
+리스트 = ['intra.h', 'intra.c', 'define.h', 'run.py']
+for i in 리스트:
+    j = i.split(".")
+    if j[1] == "h" or j[1] == "c":
+        print(i)
+        
+#161 for & range
+for i in range(2002, 2051, 4):
+    print(i)
